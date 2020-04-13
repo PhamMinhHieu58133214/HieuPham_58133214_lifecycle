8a4e2790f9a38077b2fb35bd0f0e8532d5a147ce
Phạm Minh Hieu <Hieupham101197@hotmail.com>
2020-04-13 09:25:06 +0700

    Version 1.1

--- app/src/main/java/vn/edu/ntu/HieuPham_58133214_lifecycle/MainActivity.java
+++ app/src/main/java/vn/edu/ntu/HieuPham_58133214_lifecycle/MainActivity.java
@@ -3,12 +3,67 @@ package vn.edu.ntu.HieuPham_58133214_lifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+    TextView txtTG;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("LifeCycle","onCreate Called");
+        txtTG = findViewById(R.id.txtTG);
+        Toast.makeText(getApplicationContext(), "onCreate", Toast.LENGTH_SHORT).show();
+        SimpleDateFormat spdF = new SimpleDateFormat("HH:mm:ss");
+        String date = spdF.format(new Date());
+        txtTG.setText(date);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("LifeCycle","onStart Called");
+        Toast.makeText(getApplicationContext(), "onStart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d("LifeCycle","onRestart Called");
+        Toast.makeText(getApplicationContext(), "onRestart", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("LifeCycle","onResume Called");
+        Toast.makeText(getApplicationContext(), "onResume", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("LifeCycle","onPause Called");
+        Toast.makeText(getApplicationContext(), "onPause", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("LifeCycle","onStop Called");
+        Toast.makeText(getApplicationContext(), "onStop", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("LifeCycle","onDestroy Called");
+        Toast.makeText(getApplicationContext(), "onDestroy", Toast.LENGTH_SHORT).show();
     }
 }
